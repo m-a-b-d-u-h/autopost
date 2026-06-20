0c612e0291dc3c654bd01583a6e64eb3ff2eaa50
Arch - Muhammad Abduh <imuhammadabduh@gmail.com>
2026-06-20 07:43:02 +0700

    Add ecosystem.config.cjs for PM2

---
+++ ecosystem.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "autopost",
+      script: "server.js",
+      exec_mode: "fork",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "500M",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+}
